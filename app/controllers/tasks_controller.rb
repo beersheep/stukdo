1d69b27fc71f12444db5c5f83f1a206a3958c779
Roy Huang <RoyHuang@Roys-MacBook-Air.local>
2015-02-17 19:14:47 +0800

    added state to our tasks and sort by state

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,9 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = current_user.tasks
+    @to_do = current_user.tasks.where(state: "to_do")
+    @doing = current_user.tasks.where(state: "doing")
+    @done = current_user.tasks.where(state: "done")
   end
 
   
@@ -64,6 +66,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :state)
     end
 end
